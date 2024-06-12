626d7d63bcbce0ce3ba4bc5fd3650127ff7a9ed7
oscardenas <oscardenas17@gmail.com>
2024-06-12 16:36:00 -0500

    Initial comit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <h1 className="text-6xl">ddd</h1>
+  </React.StrictMode>
+);
